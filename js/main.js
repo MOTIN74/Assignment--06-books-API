345894ac995b4808ea2d3bf1d0bbda5cb5080ac3
m@KHAN <amotin68@gmail.com>
2021-09-02 19:55:58 +0600

    m2

--- js/main.js
+++ js/main.js
@@ -4,10 +4,10 @@ const searchBooks = () => {
     const input = document.getElementById('input');
     const inputText = input.value;
     
-    if (input.value === '' && input.value.length !== null) {
+    if (input.value === '') {
         document.getElementById('searchResult').innerHTML = `<div class='position-absolute w-90 shadow-lg  d-flex flex-column justify-content-center align-items-center pt-5' style="height:300px">
         <img src="img/text-field.svg" class='img-fluid h-100'>
-        <h2 class="my-4 text-center">You've to type any Books name first</h2>
+        <h2 class="my-4 text-center" style="font-family: 'Poppins', sans-serif;">You've to type any Books name first</h2>
     </div>`;
         document.getElementById('cardContainer').textContent = '';
     } else {
@@ -40,12 +40,12 @@ const displayResult = allData => {
     if (allData.length === 0 ) {
         document.getElementById('searchResult').innerHTML = ` <div class='position-absolute w-100 d-flex flex-column justify-content-center align-items-center pt-5' style="height:300px">
         <img src="img/empty.svg" class=' h-100'>
-        <h2 class="my-4 text-center">No Books Found</h2>
+        <h2 class="my-4 text-center" style="font-family: 'Poppins', sans-serif;">No Books Found</h2>
         </div>`;
         
       } else {
         // //////for each loop start /////////////////
-        allData.forEach(data => {
+        allData?.forEach(data => {
             ////////// creating card div start//////////////
             const div = document.createElement('div');
             div.classList.add('col-md-4');
@@ -53,10 +53,10 @@ const displayResult = allData => {
             <div style="height: 500px;" class="card p-2 m-3 rounded ">
                 <img style="height: 15rem;" class="img-fluid" src="https://covers.openlibrary.org/b/id/${data.cover_i}-M.jpg" alt="Image not found" onerror="this.onerror=null;this.src='imagefound.gif';" >
                 
-                <h3>${data.title}</h3>
-                <p class="p-0 m-0"><span class="fw-bold">Author:</span> ${data.author_name}</p>
-                <p class="p-0 m-0"><span class="fw-bold">Publisher:</span> ${data.publisher}</p>
-                <p class="p-0 m-0"><span class="fw-bold">First publish :</span> ${data.first_publish_year}</p>
+                <h3 style="font-family: 'Poppins', sans-serif;">${data.title}</h3>
+                <p class="p-0 m-0" style="font-family: 'Poppins', sans-serif;"><span class="fw-bold">Author:</span> ${data.author_name}</p>
+                <p class="p-0 m-0" style="font-family: 'Poppins', sans-serif;"><span class="fw-bold">Publisher:</span> ${data.publisher}</p>
+                <p class="p-0 m-0" style="font-family: 'Poppins', sans-serif;"><span class="fw-bold">First publish :</span> ${data.first_publish_year}</p>
             </div>
             `
             //////////////// creating card div end ///////////////
